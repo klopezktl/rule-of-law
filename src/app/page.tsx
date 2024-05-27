2ffd322a70c4b8c2a1fe3f003c260f7301b93680
Kimberly Lopez <klopezktl@gmail.com>
2024-05-28 05:58:57 +0800

    Main page initial commit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
-      Main Page
+    <main className="h-screen">
+      <section className="flex h-screen justify-between">
+        <div className="mx-4 h-screen w-full bg-white sm:w-10/12">
+          <section className="ml-8 h-[545px] bg-blue-200">
+            <div></div>
+          </section>
+          <section className="mt-8 h-80 bg-blue-400"></section>
+        </div>
+        <div className="mx-4 h-screen w-full bg-white sm:w-5/12"></div>
+      </section>
     </main>
   );
 }
